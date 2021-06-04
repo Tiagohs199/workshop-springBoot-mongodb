0d783f11d3c63a13ed100c62bd9ffa9ade539bc2
Tiagohs199 <tiagohs.199@gmail.com>
2021-06-04 02:49:15 -0300

    add delete and update

--- src/main/java/com/tiago/workshopmongo/services/UserService.java
+++ src/main/java/com/tiago/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.tiago.workshopmongo.domain.User;
+import com.tiago.workshopmongo.dto.UserDTO;
 import com.tiago.workshopmongo.repository.UserRepository;
 import com.tiago.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,5 +26,30 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+		
+	}
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+		
+	}
+
+	public User fromDTO(UserDTO obj) {
+		return new User(obj.getId(), obj.getName(), obj.getEmail());
 	}
 }
