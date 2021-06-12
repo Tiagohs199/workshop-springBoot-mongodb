084c9cc2710050bebca9254d032da38e57ee7f90
Tiagohs199 <tiagohs.199@gmail.com>
2021-06-12 16:10:21 -0300

    obtendo posts by id

--- src/main/java/com/tiago/workshopmongo/resources/UserResource.java
+++ src/main/java/com/tiago/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.tiago.workshopmongo.domain.Post;
 import com.tiago.workshopmongo.domain.User;
 import com.tiago.workshopmongo.dto.UserDTO;
 import com.tiago.workshopmongo.services.UserService;
@@ -35,7 +36,6 @@ public class UserResource {
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<UserDTO> findById(@PathVariable String id){	
 		User obj = service.findById(id);
-		
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 	
@@ -62,6 +62,12 @@ public class UserResource {
 		obj = service.update(obj);
 		
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){	
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 	
 }
