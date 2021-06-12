6f660c691c7044e9e0408b33dda4759f4cce3998
Tiagohs199 <tiagohs.199@gmail.com>
2021-06-12 19:03:34 -0300

    Consulta simples com query methods

--- src/main/java/com/tiago/workshopmongo/config/Instantiation.java
+++ src/main/java/com/tiago/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.tiago.workshopmongo.domain.Post;
 import com.tiago.workshopmongo.domain.User;
 import com.tiago.workshopmongo.dto.AuthorDTO;
+import com.tiago.workshopmongo.dto.CommentDTO;
 import com.tiago.workshopmongo.repository.PostRepository;
 import com.tiago.workshopmongo.repository.UserRepository;
 
@@ -43,8 +44,16 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("07/06/2021"),"Let's go", "Lets go to Amsterdam",new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("02/06/2021"),"Ces't parti", "Ces't parti pour Amsterdam",new AuthorDTO(maria));
+		Post post3 = new Post(null, sdf.parse("02/06/2021"),"Ce o que", "Ces't parti pour Amsterdam",new AuthorDTO(alex));
 		
-		postReposiroty.saveAll(Arrays.asList(post1,post2));
+		CommentDTO c1 = new CommentDTO("Let's go Bro", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("bon voiage", sdf.parse("24/06/2017"), new AuthorDTO(alex));
+		CommentDTO c3 = new CommentDTO("Partiu mano", sdf.parse("27/04/2014"), new AuthorDTO(bob));
+		
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
+		postReposiroty.saveAll(Arrays.asList(post1,post2,post3));
 		
 		maria.getPosts().addAll(Arrays.asList(post1,post2));
 		userReposiroty.save(maria);
